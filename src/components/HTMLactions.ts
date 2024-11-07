caa4b1e7de9eeade90a31b8291c6331da0abfec9
Liza Bragina <catherineby123@yandex.ru>
2024-11-08 01:34:14 +0300

    родитель

---
+++ src/components/HTMLactions.ts
@@ -0,0 +1,50 @@
+import { IEvents } from "./base/events";
+
+export class HTMLAction<T> {
+    constructor(protected readonly container: HTMLElement) {}
+
+    toggleClass(className: string): void {
+        this.container.classList.toggle(className);
+    }
+
+    protected setText(element: HTMLElement, value: unknown): void {
+        if (element) {
+            element.textContent = String(value);
+        }
+    }
+
+    protected setImage(element: HTMLImageElement, src: string, alt?: string): void {
+        if (element) {
+            element.src = src;
+            if (alt) {
+                element.alt = alt;
+            }
+        }
+    }
+
+    render(data?: T): HTMLElement {
+        Object.assign(this as object, data ?? {});
+        return this.container;
+    }
+}
+
+export class View<T> extends HTMLAction<T> {
+    constructor(protected readonly events: IEvents, container: HTMLElement) {
+        super(container);
+    }
+    updateBasketCount(count: number): void {
+        const basketCounter = this.container.querySelector('.header__basket-counter');
+        if (basketCounter) {
+            this.setText(basketCounter as HTMLElement, count);
+        }
+    }
+    showOrderSuccess(amount: number): void {
+        const successTemplate = document.getElementById('success')?.innerHTML;
+        if (successTemplate) {
+            const successElement = document.createElement('div');
+            successElement.innerHTML = successTemplate;
+            this.setText(successElement.querySelector('.order-success__description') as HTMLElement, `Списано ${amount} синапсов`);
+            this.container.appendChild(successElement);
+        }
+    }
+}
