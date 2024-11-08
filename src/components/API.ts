a5502ced918daec2611f41df64869d80ea272fcf
Liza Bragina <catherineby123@yandex.ru>
2024-11-08 23:31:07 +0300

    АПИ

--- src/components/API.ts
+++ src/components/API.ts
@@ -1,37 +1,37 @@
 import { Api, ApiListResponse } from '../components/base/api'; 
-import { OrderInput, OrderOutcome, ProductDetails } from '../types';
+import { OrderDetails, OrderOutcome, ProductDetails } from '../types';
 
 export interface ProductAPI {
     getAllProducts: () => Promise<ProductDetails[]>;
-    getSingleProduct: (productId: string) => Promise<ProductDetails>;
-    createOrder: (orderData: OrderInput) => Promise<OrderOutcome>;
+    getSingleProduct: (id: string) => Promise<ProductDetails>;
+    createOrder: (order: OrderDetails) => Promise<OrderOutcome>;
 }
 
 export class ProductAPIService extends Api implements ProductAPI {
     private readonly contentDeliveryBase: string;
 
-    constructor(cdnUrl: string, baseApiUrl: string, requestOptions?: RequestInit) {
-        super(baseApiUrl, requestOptions);
-        this.contentDeliveryBase = cdnUrl;
+    constructor(cdn: string, baseUrl: string, options?: RequestInit) {
+        super(baseUrl, options);
+        this.contentDeliveryBase = cdn;
     }
 
-    getSingleProduct(productId: string): Promise<ProductDetails> {
-        return this.get(`/product/${productId}`).then((productData: ProductDetails) => ({
-            ...productData,
-            image: this.contentDeliveryBase + productData.image,
+    getSingleProduct(id: string): Promise<ProductDetails> {
+        return this.get(`/product/${id}`).then((data: ProductDetails) => ({
+            ...data,
+            image: this.contentDeliveryBase + data.image,
         }));
     }
 
     getAllProducts(): Promise<ProductDetails[]> {
-        return this.get('/product').then((response: ApiListResponse<ProductDetails>) => {
-            return response.items.map(item => ({
+        return this.get('/product').then((data: ApiListResponse<ProductDetails>) => {
+            return data.items.map(item => ({
                 ...item,
                 image: this.contentDeliveryBase + item.image,
             }));
         });
     }
 
-    createOrder(orderData: OrderInput): Promise<OrderOutcome> {
-        return this.post('/order', orderData).then((response: OrderOutcome) => response);
+    createOrder(order: OrderDetails): Promise<OrderOutcome> {
+        return this.post('/order', order).then((data: OrderOutcome) => data);
     }
 }
